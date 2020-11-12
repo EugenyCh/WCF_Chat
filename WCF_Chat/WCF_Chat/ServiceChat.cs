dd4af5b2094f1e5fe6223fda0b5f1a7af5eb0226
Eugeny Chekmarev <eugeny007@gmail.com>
2020-11-12 12:24:19 +0300

    wpf_chat

--- WCF_Chat/WCF_Chat/ServiceChat.cs
+++ WCF_Chat/WCF_Chat/ServiceChat.cs
@@ -10,19 +10,46 @@ namespace WCF_Chat
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
     public class ServiceChat : IServiceChat
     {
-        public int Connect()
+        List<ServerUser> users = new List<ServerUser>();
+        int nextId = 1;
+
+        public int Connect(string name)
         {
-            throw new NotImplementedException();
+            ServerUser user = new ServerUser()
+            {
+                ID = nextId++,
+                Name = name,
+                operationContext = OperationContext.Current
+            };
+            SendMessage($"{user.Name} подключился к чату!", 0);
+            users.Add(user);
+            return user.ID;
         }
 
         public void Disconnect(int id)
         {
-            throw new NotImplementedException();
+            var user = users.FirstOrDefault(u => u.ID == id);
+            if (user != null)
+            {
+                users.Remove(user);
+                SendMessage($"{user.Name} покинул чат!", 0);
+                --nextId;
+            }
         }
 
-        public void SendMessage(string message)
+        public void SendMessage(string message, int id)
         {
-            throw new NotImplementedException();
+            foreach (var item in users)
+            {
+                string answer = DateTime.Now.ToShortTimeString();
+
+                var user = users.FirstOrDefault(u => u.ID == id);
+                if (user != null)
+                    answer += ": " + user.Name + " ";
+                answer += message;
+
+                item.operationContext.GetCallbackChannel<IServerChatCallback>().MessageCallback(answer);
+            }
         }
     }
 }
