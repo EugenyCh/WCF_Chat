fb59fbe21b744589a4f58553b0edb163bd7bb402
Eugeny Chekmarev <eugeny007@gmail.com>
2020-11-12 11:55:06 +0300

    first commit

---
+++ WCF_Chat/WCF_Chat/IServiceChat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Chat
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IServiceChat" в коде и файле конфигурации.
+    [ServiceContract(CallbackContract = typeof(IServerChatCallback))]
+    public interface IServiceChat
+    {
+        [OperationContract]
+        int Connect();
+
+        [OperationContract]
+        void Disconnect(int id);
+
+        [OperationContract(IsOneWay = true)]
+        void SendMessage(string message);
+    }
+
+    public interface IServerChatCallback
+    {
+        [OperationContract]
+        void MessageCallback(string message);
+    }
+}
